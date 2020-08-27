2b0b207ce7cc800e9ecffebcdde4d3fe8f309b4a
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2020-08-26 23:15:52 -0400

    Update Script

--- request/utils.sh
+++ request/utils.sh
@@ -179,7 +179,7 @@ iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
 iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
 iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP' >> ./torrent-adm
-./torrent-adm && rm ./torrent-adm
+./torrent-adm
 echo "#TORRENT-ON" > /etc/torrent-on
 msg -bar
 echo -e " $(fun_trans "Aplicado!")"

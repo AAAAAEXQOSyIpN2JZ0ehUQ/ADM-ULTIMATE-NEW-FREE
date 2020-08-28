074e7e0b28541f108b93d505fae8c9b8c60ebbbf
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2020-08-28 18:22:18 -0400

    Update Script

--- Install/visorpuertos.sh
+++ Install/visorpuertos.sh
@@ -6,6 +6,17 @@ SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
 SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
 SCPidioma="${SCPdir}/idioma" && [[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
 
+meu_ip () {
+if [[ -e /etc/MEUIPADM ]]; then
+echo "$(cat /etc/MEUIPADM)"
+else
+MEU_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+MEU_IP2=$(wget -qO- ipv4.icanhazip.com)
+[[ "$MEU_IP" != "$MEU_IP2" ]] && echo "$MEU_IP2" || echo "$MEU_IP"
+echo "$MEU_IP2" > /etc/MEUIPADM
+fi
+}
+
 echo -e "\033[1;33m INFORMACION DE SISTEMAS"
 echo -e "$barra"
 
--- request/statusdesistema.sh
+++ request/statusdesistema.sh
@@ -6,16 +6,18 @@ SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
 SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
 SCPidioma="${SCPdir}/idioma" && [[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
 
-#Repositorio-GITHUB
-[[ ! -d /etc/ger-tools ]] && mkdir /etc/ger-tools > /dev/null 2>&1
-link_bin="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/ADM-ULTIMATE-NEW-FREE/master/Install/ger-tools/htop.sh"
-[[ ! -e /etc/ger-tools/htop.sh ]] && wget -O /etc/ger-tools/htop.sh ${link_bin} > /dev/null 2>&1 && chmod +x /etc/ger-tools/htop.sh
-link_bin="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/ADM-ULTIMATE-NEW-FREE/master/Install/ger-tools/nload.sh"
-[[ ! -e /etc/ger-tools/nload.sh ]] && wget -O /etc/ger-tools/nload.sh ${link_bin} > /dev/null 2>&1 && chmod +x /etc/ger-tools/nload.sh
-link_bin="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/ADM-ULTIMATE-NEW-FREE/master/Install/ger-tools/visorpuertos.sh"
-[[ ! -e /etc/ger-tools/visorpuertos.sh ]] && wget -O /etc/ger-tools/visorpuertos.sh ${link_bin} > /dev/null 2>&1 && chmod +x /etc/ger-tools/visorpuertos.sh
-link_bin="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/ADM-ULTIMATE-NEW-FREE/master/Install/ger-tools/nettools"
-[[ ! -e /etc/ger-tools/nettools ]] && wget -O /etc/ger-tools/nettools ${link_bin} > /dev/null 2>&1 && chmod +x /etc/ger-tools/nettools
+[[ -d /etc/ger-tools ]] && rm -rf /etc/ger-tools
+
+meu_ip () {
+if [[ -e /etc/MEUIPADM ]]; then
+echo "$(cat /etc/MEUIPADM)"
+else
+MEU_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+MEU_IP2=$(wget -qO- ipv4.icanhazip.com)
+[[ "$MEU_IP" != "$MEU_IP2" ]] && echo "$MEU_IP2" || echo "$MEU_IP"
+echo "$MEU_IP2" > /etc/MEUIPADM
+fi
+}
 
 fun_bar () {
 comando="$1"
@@ -40,19 +42,113 @@ sleep 1s
 }
 
 fun_nload () {
-/etc/ger-tools/nload.sh
+echo -e "${cor[3]} $(fun_trans "Con nload puedes ver todos el trafico")"
+echo -e "${cor[3]} $(fun_trans "de red generado en tu sistema")"
+echo -e "${cor[4]} $(fun_trans "PARA SALIR DEL PANEL PRESIONE") ${cor[3]}CTLR+C"
+msg -bar
+sleep 1s
+fun_bar "apt-get install nload -y"
+sleep 2s
+nload
 }
 
 fun_htop () {
-/etc/ger-tools/htop.sh
+echo -e "${cor[3]} $(fun_trans "Con htop puedes ver todos los procesos")"
+echo -e "${cor[3]} $(fun_trans "que se ejecutan en tu sistema")"
+echo -e "${cor[4]} $(fun_trans "PARA SALIR DEL PANEL PRESIONE") ${cor[3]}CTLR+C"
+msg -bar
+sleep 1s
+fun_bar "apt-get install htop -y"
+sleep 2s
+htop
 }
 
 fun_visorpuertos () {
-/etc/ger-tools/visorpuertos.sh
+echo -e "\033[1;33m INFORMACION DE SISTEMAS"
+echo -e "$barra"
+###########################################
+# SISTEMA OPERACIONAL
+_hora=$(printf '%(%H:%M:%S)T')
+_hoje=$(date +'%d/%m/%Y')
+if [ -f /etc/lsb-release ]
+then
+echo -e "\033[1;33m SISTEMA OPERACIONAL"
+echo ""
+name=$(cat /etc/lsb-release |grep DESCRIPTION |awk -F = {'print $2'})
+codename=$(cat /etc/lsb-release |grep CODENAME |awk -F = {'print $2'})
+echo -e "\033[1;31mNome: \033[1;37m$name"
+echo -e "\033[1;31mIP: \033[1;37m$(meu_ip)"
+echo -e "\033[1;31mHora : \033[1;37m$_hora"
+echo -e "\033[1;31mData: \033[1;37m$_hoje"
+echo -e "\033[1;31mCodeName: \033[1;37m$codename"
+echo -e "\033[1;31mKernel: \033[1;37m$(uname -s)"
+echo -e "\033[1;31mKernel Release: \033[1;37m$(uname -r)"
+if [ -f /etc/os-release ]
+then
+devlike=$(cat /etc/os-release |grep LIKE |awk -F = {'print $2'})
+echo -e "\033[1;31mDerivado do OS: \033[1;37m$devlike"
+fi
+else
+system=$(cat /etc/issue.net)
+echo -e "\033[1;33m SISTEMA OPERACIONAL"
+echo ""
+echo -e "\033[1;31mNome: \033[1;37m$system"
+fi
+###########################################
+# PROCESSADOR
+echo -e "$barra"
+if [ -f /proc/cpuinfo ]
+then
+uso=$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')
+echo -e "\033[1;33m PROCESSADOR"
+echo ""
+modelo=$(cat /proc/cpuinfo |grep "model name" |uniq |awk -F : {'print $2'})
+cpucores=$(grep -c cpu[0-9] /proc/stat)
+cache=$(cat /proc/cpuinfo |grep "cache size" |uniq |awk -F : {'print $2'})
+echo -e "\033[1;31mModelo:\033[1;37m$modelo"
+echo -e "\033[1;31mNucleos:\033[1;37m $cpucores"
+echo -e "\033[1;31mMemoria Cache:\033[1;37m$cache"
+echo -e "\033[1;31mArquitetura: \033[1;37m$(uname -p)"
+echo -e "\033[1;31multilizacao: \033[37m$uso"
+else
+echo -e "\033[1;33m PROCESSADOR"
+echo ""
+echo "Não foi possivel obter informações"
+fi
+###########################################
+# MEMORIA RAM
+echo -e "$barra"
+if free 1>/dev/null 2>/dev/null
+then
+ram1=$(free -h | grep -i mem | awk {'print $2'})
+ram2=$(free -h | grep -i mem | awk {'print $4'})
+ram3=$(free -h | grep -i mem | awk {'print $3'})
+usoram=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+echo -e "\033[1;33m MEMORIA RAM"
+echo ""
+echo -e "\033[1;31mTotal: \033[1;32m$ram1"
+echo -e "\033[1;31mEm Uso: \033[1;32m$ram3"
+echo -e "\033[1;31mLivre: \033[1;32m$ram2"
+echo -e "\033[1;31multilizacao: \033[32m$usoram"
+else
+echo -e "\033[1;33mMEMORIA RAM"
+echo ""
+echo "Não foi possivel obter informações"
+fi
+###########################################
+# SERVICOS EM EXECUCAO
+echo -e "$barra"
+echo -e "\033[1;33m SERVICOS EM EXECUCAO"
+echo ""
+PT=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN")
+for porta in `echo -e "$PT" | cut -d: -f2 | cut -d' ' -f1 | uniq`; do
+    svcs=$(echo -e "$PT" | grep -w "$porta" | awk '{print $1}' | uniq)
+    echo -e "\033[1;31m$svcs: \033[1;32m$porta"
+done
 }
 
 fun_nettools () {
-/etc/ger-tools/nettools
+/bin/nettools
 }
 
 msg -ama "$(fun_trans "STATUS DE SISTEMA") ${cor[4]}[NEW-ADM]"

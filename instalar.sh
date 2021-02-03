84bcdb3482d73a7517bb6231f6e21a0c28fc3884
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-02-03 02:03:29 -0300

    Update Script

--- instalar.sh
+++ instalar.sh
@@ -159,7 +159,7 @@ Key="qra-atsilK?29@%6087%?66d5K8888:%05+08+@@?+91"
 REQUEST=$(echo $SCPresq|$SUB_DOM)
 IP="104.238.135.147" && echo "$IP" > /usr/bin/vendor_code
 cd $HOME
-msg -ne "Key: "
+msg -ne "Files: "
 wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1;32m Verified" || {
    echo -e "\033[1;32m Verified"
    invalid_key

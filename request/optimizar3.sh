ca98b4f789b98e64fadc2c6730d798a2d372074a
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-03-13 01:23:13 -0300

    Update Script

--- request/optimizar3.sh
+++ request/optimizar3.sh
@@ -7,33 +7,31 @@ SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
 SCPidioma="${SCPdir}/idioma" && [[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
 
 fun_bar () {
-comando[0]="$1"
-comando[1]="$2"
- (
-[[ -e $HOME/fim ]] && rm $HOME/fim
-${comando[0]} -y > /dev/null 2>&1
-${comando[1]} -y > /dev/null 2>&1
-touch $HOME/fim
- ) > /dev/null 2>&1 &
- tput civis
-echo -ne "  \033[1;33m["
-while true; do
-   for((i=0; i<18; i++)); do
-   echo -ne "\033[1;31m#"
-   sleep 0.1s
+comando="$1"
+ _=$(
+$comando > /dev/null 2>&1
+) & > /dev/null
+pid=$!
+while [[ -d /proc/$pid ]]; do
+echo -ne " \033[1;33m["
+   for((i=0; i<10; i++)); do
+   echo -ne "\033[1;31m##"
+   sleep 0.2
    done
-   [[ -e $HOME/fim ]] && rm $HOME/fim && break
-   echo -e "\033[1;33m]"
-   sleep 1s
-   tput cuu1
-   tput dl1
-   echo -ne " \033[1;33m["
+echo -ne "\033[1;33m]"
+sleep 1s
+echo
+tput cuu1 && tput dl1
 done
-echo -e "\033[1;33m]\033[1;37m -\033[1;32m 100% \033[1;37m"
-tput cnorm
+echo -e " \033[1;33m[\033[1;31m####################\033[1;33m] - \033[1;32m100%\033[0m"
+sleep 1s
 }
 
 fun_optimizer () {
+[[ $(grep -wc mlocate /var/lib/dpkg/statoverride) != '0' ]] && sed -i '/mlocate/d' /var/lib/dpkg/statoverride
+msg -ama " $(fun_trans "LIMPAR MEMORIA ") \033[1;32mRAM \033[1;33mE \033[1;32mSWAP"
+msg -bar
+fun_clean () {
 # Actualizando servicios
 apt-get update -y
 apt-get upgrade -y
@@ -46,11 +44,7 @@ apt-get autoclean -y
 apt-get -f remove -y
 apt-get clean -y
 }
-
-[[ $(grep -wc mlocate /var/lib/dpkg/statoverride) != '0' ]] && sed -i '/mlocate/d' /var/lib/dpkg/statoverride
-echo -e "\033[1;33m $(fun_trans "LIMPAR MEMORIA ") \033[1;32mRAM \033[1;33mE \033[1;32mSWAP"
-msg -bar
-fun_bar "fun_optimizer"
+fun_bar "fun_clean"
 msg -bar
 MEM1=$(free | awk '/Mem:/ {print int(100*$3/$2)}')
 ram1=$(free -h | grep -i mem | awk {'print $2'})
@@ -86,8 +80,10 @@ swap1=$(free -h | grep -i swap | awk {'print $2'})
 swap2=$(free -h | grep -i swap | awk {'print $4'})
 swap3=$(free -h | grep -i swap | awk {'print $3'})
 # msg -bar
-echo -e "\033[1;37m $(fun_trans "Economia de"):\033[1;31m $(expr $MEM1 - $MEM2)%\033[0m"
+msg -bra " $(fun_trans "Economia de"):\033[1;31m $(expr $MEM1 - $MEM2)%\033[0m"
 msg -bar
-echo -e "${cor[3]} $(fun_trans "PROCESSO CONCLUIDO")"
+msg -ama " $(fun_trans "PROCESSO CONCLUIDO")"
 msg -bar
+}
+fun_optimizer
 #fin

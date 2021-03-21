dcde60ef397db10d50fc1de0caa142347110224c
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-03-21 16:50:44 -0300

    Update Script

--- request/gestor.sh
+++ request/gestor.sh
@@ -92,17 +92,17 @@ return
 cambiopass () {
 echo -e "${cor[3]} $(fun_trans "Esta herramienta cambia la contraseña de su servidor vps")"
 echo -e "${cor[3]} $(fun_trans "Esta contraseña es utilizada como usuario") root"
-echo -e "$barra"
+msg -bar
 echo -ne " $(fun_trans "Desea Seguir?") [S/N]: "; read x
 [[ $x = @(n|N) ]] && return
 #Inicia Procedimentos
-echo -e "$barra"
+msg -bar
 echo -e "\033[1;37m $(fun_trans "Escriba su nueva contraseña")"
-echo -e "$barra"
+msg -bar
 read  -p " Nuevo passwd: " pass
 (echo $pass; echo $pass)|passwd 2>/dev/null
 sleep 1s
-echo -e "$barra"
+msg -bar
 echo -e "${cor[3]} $(fun_trans "Contraseña cambiada con exito!")"
 echo -e "${cor[2]} $(fun_trans "Su contraseña ahora es"): ${cor[4]}$pass"
 return
@@ -121,6 +121,11 @@ return
 }
 
 cleanreg () {
+if [[ ! -e /etc/newadm/ger-user/Limiter.log ]]; then
+msg -ama " Limiter.log No Encontrado"
+msg -bar
+exit 1
+fi
 echo -ne " \033[1;31m[ ! ] Registro del limitador eliminado"
 sudo rm -rf /etc/newadm/ger-user/Limiter.log > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
 return
@@ -128,60 +133,55 @@ return
 
 pamcrack () {
 echo -e "${cor[3]} $(fun_trans "Liberar passwd para VURTL")"
-echo -e "$barra"
+msg -bar
 echo -ne " $(fun_trans "Desea Seguir?") [S/N]: "; read x
-[[ $x = @(n|N) ]] && echo -e "$barra" && return
-echo -e ""
+[[ $x = @(n|N) ]] && return
+msg -bar
 fun_bar "service ssh restart"
 sed -i 's/.*pam_cracklib.so.*/password sufficient pam_unix.so sha512 shadow nullok try_first_pass #use_authtok/' /etc/pam.d/common-password
-fun_bar "service ssh restart"
-echo -e ""
+service ssh restart > /dev/null 2>&1
+msg -bar
 echo -e " \033[1;31m[ ! ]\033[1;33m $(fun_trans "Configuraciones VURTL aplicadas")"
 return
 }
 
 rootpass () {
-echo -e "${cor[3]} $(fun_trans "Esta herramienta cambia a usuario root las vps de ")"
-echo -e "${cor[3]} $(fun_trans "Googlecloud y Amazon esta configuracion solo")"
-echo -e "${cor[3]} $(fun_trans "funcionan en Googlecloud y Amazon Puede causar")"
-echo -e "${cor[3]} $(fun_trans "error en otras VPS agenas a Googlecloud y Amazon ")"
-echo -e "$barra"
-echo -ne " $(fun_trans "Desea Seguir?") [S/N]: "; read x
-[[ $x = @(n|N) ]] && echo -e "$barra" && return
-echo -e "$barra"
+echo -e "${cor[3]} $(fun_trans "Esta herramienta cambia a usuario root las VPS de")"
+echo -e "${cor[3]} $(fun_trans "GoogleCloud y Amazon")"
+msg -bar
+echo -ne " Desea Seguir? [S/N]: "; read x
+[[ $x = @(n|N) ]] && return
+msg -bar
 #Inicia Procedimentos
-echo -e "${cor[0]} $(fun_trans "Aplicando Configuracoes")"
 fun_bar "service ssh restart"
 #Parametros Aplicados
 sed -i "s;PermitRootLogin prohibit-password;PermitRootLogin yes;g" /etc/ssh/sshd_config
 sed -i "s;PermitRootLogin without-password;PermitRootLogin yes;g" /etc/ssh/sshd_config
 sed -i "s;PasswordAuthentication no;PasswordAuthentication yes;g" /etc/ssh/sshd_config
-echo -e "$barra"
-echo -e "${cor[0]} $(fun_trans "Escriba su contraseña root actual")"
-echo -e "${cor[0]} $(fun_trans "si lo decea puede cambiarla")"
-echo -e "$barra"
+msg -bar
+echo -e "\033[1;37m $(fun_trans "Escriba su nueva contraseña")"
+msg -bar
 read  -p " Nuevo passwd: " pass
 (echo $pass; echo $pass)|passwd 2>/dev/null
 sleep 1s
-echo -e "$barra"
-echo -e "${cor[3]} $(fun_trans "Configuraciones aplicadas con exito!")"
+msg -bar
+echo -e "${cor[3]} $(fun_trans "Contraseña cambiada con exito!")"
 echo -e "${cor[2]} $(fun_trans "Su contraseña ahora es"): ${cor[4]}$pass"
-service ssh restart > /dev/null 2>&1
 return
 }
 
-msg -ama " $(fun_trans "GERENCIAR SERVIDOR VPS")"
+msg -ama " $(fun_trans "GERENCIAR SISTEMA INTERNO")"
 msg -bar
 echo -ne "\033[1;32m [0] > " && msg -bra "$(fun_trans "VOLTAR")"
 echo -ne "\033[1;32m [1] > " && msg -azu "$(fun_trans "ATUALIZAR PACOTES")"
 echo -ne "\033[1;32m [2] > " && msg -azu "$(fun_trans "REINICIAR OS SERVICO")"
 echo -ne "\033[1;32m [3] > " && msg -azu "$(fun_trans "REINICIAR SISTEMA")"
 echo -ne "\033[1;32m [4] > " && msg -azu "$(fun_trans "ALTERAR O NOME DO SISTEMA")"
-echo -ne "\033[1;32m [5] > " && msg -azu "$(fun_trans "CAMBIAR CONTRASEñA ROOT DEL SISTEMA")"
+echo -ne "\033[1;32m [5] > " && msg -azu "$(fun_trans "CAMBIAR CONTRASEÑA ROOT DEL SISTEMA")"
 echo -ne "\033[1;32m [6] > " && msg -azu "$(fun_trans "ATUALIZAR HORA AMERICA-SANTIAGO")"
 echo -ne "\033[1;32m [7] > " && msg -azu "$(fun_trans "ELIMINAR REGISTRO DEL LIMITADO") \033[1;33m(\033[1;37mBETA\033[1;33m)"
 echo -ne "\033[1;32m [8] > " && msg -azu "$(fun_trans "DESBLOQUEAR VURTL PARA CREAR USUARIOS") \033[1;33m(\033[1;37mBETA\033[1;33m)"
-echo -ne "\033[1;32m [9] > " && msg -azu "$(fun_trans "SERVICO ROOT PARA GOOGLECLOUD E AMAZON") \033[1;33m(\033[1;37mBETA\033[1;33m)"
+echo -ne "\033[1;32m [9] > " && msg -azu "$(fun_trans "APLICAR ROOT A GOOGLECLOUD Y AMAZON") \033[1;33m(\033[1;37mBETA\033[1;33m)"
 msg -bar
 while [[ ${arquivoonlineadm} != @(0|[1-9]) ]]; do
 read -p "[0-9]: " arquivoonlineadm

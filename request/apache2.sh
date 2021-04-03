1c519f63f7cde192dee52c050e07e6910a1e240b
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-04-02 21:25:01 -0300

    Update Script

--- request/apache2.sh
+++ request/apache2.sh
@@ -130,6 +130,7 @@ msg -bar
 fun_bar "apt-get install apache2 -y"
 sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 sleep 0.5s
+msg -bar
 msg -ne "\033[1;31m [ ! ] \033[1;33m$(fun_trans "REINICIANDO SERVICOS")"
 service apache2 restart > /dev/null 2>&1 &
 echo -e "\033[1;32m[OK]"

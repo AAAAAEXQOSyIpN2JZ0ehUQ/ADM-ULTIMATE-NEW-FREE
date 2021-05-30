0a510f8fe22bc606707a01dceebf41ee8d1e0e2b
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-05-30 16:29:20 -0400

    Update Script

--- instalar.sh
+++ instalar.sh
@@ -13,15 +13,8 @@ SUB_DOM='base64 -d'
 rm $(pwd)/$0 &> /dev/null
 
 msg () {
-BRAN='\033[1;37m' && 
-VERMELHO='\e[31m' && 
-VERDE='\e[32m' && 
-AMARELO='\e[33m'
-AZUL='\e[34m' && 
-MAGENTA='\e[35m' && 
-MAG='\033[1;36m' && 
-NEGRITO='\e[1m' && 
-SEMCOR='\e[0m'
+BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
+AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCOR='\e[0m'
  case $1 in
   -ne)cor="${VERMELHO}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
   -ama)cor="${AMARELO}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;

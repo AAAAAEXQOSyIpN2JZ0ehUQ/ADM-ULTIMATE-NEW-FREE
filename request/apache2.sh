901f410beff2acd6a99717fac40c90cf8991cd72
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-07-05 12:49:38 -0400

    Update Script

--- request/apache2.sh
+++ request/apache2.sh
@@ -77,7 +77,9 @@ sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 sleep 0.5s
 msg -ne "\033[1;31m [ ! ] \033[1;33m$(fun_trans "REINICIANDO SERVICOS")"
 service apache2 restart > /dev/null 2>&1 &
-echo -e "\033[1;32m[OK] \033[1;33m$(fun_trans "PORTA 81 POR PADRAO")"
+echo -e " \033[1;32m[OK]"
+msg -bar
+echo -e "033[1;33m$(fun_trans "APACHE2 PORTA 81 POR PADRAO ")"
 msg -bar
 sleep 0.5s
 msg -ama " $(fun_trans "Sucesso Procedimento Feito")"
